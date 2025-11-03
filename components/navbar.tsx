cc77f636ba44daa7b14d091bd5022c5fb0e2b7c3
Karim Badwork <karim@badwork.fr>
2025-11-03 02:59:20 +0100

    fix: supprimer le composant ThemeSwitch et son import dans Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,7 +16,7 @@ import NextLink from "next/link";
 import clsx from "clsx";
 
 import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
+// import { ThemeSwitch } from "@/components/theme-switch";
 import {
   TwitterIcon,
   GithubIcon,
@@ -110,7 +110,7 @@ export const Navbar = () => {
         <Link isExternal aria-label="Github" href={siteConfig.links.github}>
           <GithubIcon className="text-default-500" />
         </Link>
-        <ThemeSwitch />
+        {/* <ThemeSwitch /> */}
         <NavbarMenuToggle />
       </NavbarContent>
 
