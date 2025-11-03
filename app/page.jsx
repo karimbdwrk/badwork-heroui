03b188bfd538a980a7164e52927577244f4b7499
Karim Badwork <karim@badwork.fr>
2025-11-03 20:39:52 +0100

    feat: remplacer la section de contact par le composant ContactSection dans Home

--- app/page.jsx
+++ app/page.jsx
@@ -1,35 +1,11 @@
-import { Input, Textarea } from "@heroui/input";
-import { Button } from "@heroui/button";
 import HeroSection from "@/components/HeroSection";
+import ContactSection from "@/components/ContactSection";
 
 export default function Home() {
 	return (
 		<>
 			<HeroSection />
-
-			<div
-				className='contact-section h-screen w-screen overflow-hidden flex flex-col items-center justify-center'
-				id='contact'>
-				<div className='contact-form'>
-					<div className='flex flex-col w-full flex-wrap md:flex-nowrap gap-4'>
-						<Input
-							isRequired
-							className='max-w-xs'
-							label='Email'
-							type='email'
-							placeholder='Enter your email'
-						/>
-						<Textarea
-							isRequired
-							className='max-w-xs'
-							label='Description'
-							labelPlacement='inside'
-							placeholder='Enter your description'
-						/>
-						<Button color='primary'>Send</Button>
-					</div>
-				</div>
-			</div>
+			<ContactSection />
 		</>
 	);
 }
