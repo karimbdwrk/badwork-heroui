f4fd7c04567954a7b5115d4b5cbf49e646f48539
Karim Badwork <karim@badwork.fr>
2025-11-03 03:01:07 +0100

    fix: commenter le composant ThemeSwitch dans Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -89,7 +89,7 @@ export const Navbar = () => {
           <Link isExternal aria-label="Github" href={siteConfig.links.github}>
             <GithubIcon className="text-default-500" />
           </Link>
-          <ThemeSwitch />
+          {/* <ThemeSwitch /> */}
         </NavbarItem>
         <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
         <NavbarItem className="hidden md:flex">
