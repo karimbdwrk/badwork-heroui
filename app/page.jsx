0b3aec44e9c3bb6901da7164bf537adb385f6d59
Karim Badwork <karim@badwork.fr>
2025-11-03 19:48:25 +0100

    refactor: simplifier le composant Home en supprimant le code inutilisé et en intégrant HeroSection

--- app/page.jsx
+++ app/page.jsx
@@ -1,77 +1,34 @@
-"use client";
-
-import dynamic from "next/dynamic";
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
-
-import { Input } from "@heroui/input";
-import { Button, ButtonGroup } from "@heroui/button";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-
-// import Dither from "../components/Dither";
-const Dither = dynamic(() => import("@/components/Dither"), { ssr: false });
-
-import { russoOne } from "./fonts";
-
-import styles from "../styles/page.module.css";
+import { Input, Textarea } from "@heroui/input";
+import { Button } from "@heroui/button";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
-	const scrollToSection = (id) => {
-		const element = document.getElementById(id);
-		if (element) {
-			element.scrollIntoView({
-				behavior: "smooth",
-				block: "start",
-			});
-		}
-	};
-
 	return (
 		<>
-			<div className='hero-container h-screen w-screen overflow-hidden flex flex-col items-center justify-center'>
-				<div
-					style={{
-						width: "100vw",
-						height: "100vh",
-						position: "absolute",
-						zIndex: -1,
-					}}>
-					<Dither
-						waveColor={[0.5, 0.5, 0.5]}
-						disableAnimation={false}
-						enableMouseInteraction={true}
-						mouseRadius={0.3}
-						colorNum={4}
-						waveAmplitude={0.3}
-						waveFrequency={3}
-						waveSpeed={0.05}
-					/>
-				</div>
-				<div className='intro'>
-					<h1
-						className={`${russoOne.className} text-9xl font-bold hero-title`}>
-						BADWORK
-					</h1>
-					<p>
-						Design and creation converge to shape human-centered
-						experiences.
-					</p>
-					<Button
-						color='primary'
-						onPress={() => scrollToSection("contact")}>
-						CONTACT US
-					</Button>
-				</div>
-			</div>
+			<HeroSection />
+
 			<div
 				className='contact-section h-screen w-screen overflow-hidden flex flex-col items-center justify-center'
 				id='contact'>
-				<p>Contact section</p>
+				<div className='contact-form'>
+					<div className='flex flex-col w-full flex-wrap md:flex-nowrap gap-4'>
+						<Input
+							isRequired
+							className='max-w-xs'
+							label='Email'
+							type='email'
+							placeholder='Enter your email'
+						/>
+						<Textarea
+							isRequired
+							className='max-w-xs'
+							label='Description'
+							labelPlacement='inside'
+							placeholder='Enter your description'
+						/>
+						<Button color='primary'>Send</Button>
+					</div>
+				</div>
 			</div>
 		</>
 	);
