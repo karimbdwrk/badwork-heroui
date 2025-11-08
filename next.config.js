d15548a94a22f17fc3db370d3e4cc06890bb3d00
Karim Badwork <karim@badwork.fr>
2025-11-09 00:43:38 +0100

    feat: corriger la clé d'environnement dans next.config.js pour la configuration de Supabase

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
 	},
 	env: {
 		NEXT_PUBLIC_SUPABASE_URL: "https://wmxnxnwivbmywhnsqgya.supabase.co",
-		SUPABASE_SERVICE_ROLE_KEY:
+		NEXT_PUBLIC_SUPABASE_ANON_KEY:
 			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndteG54bndpdmJteXdobnNxZ3lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2NjE1MTksImV4cCI6MjA3NzIzNzUxOX0.zB1C1k8_ytIvIR_lLjh74fB7dc8sAJon93bb-I-vjLo",
 		RESEND_API_KEY: "re_YztwPX4R_7FAw6NyiJqJiMB7iTETZkPgm",
 		EMAIL_TO: "karim@badwork.fr",
