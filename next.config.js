7b2e86ee2784c738d06a310c8ce4df29cd47c1c6
Karim Badwork <karim@badwork.fr>
2025-12-19 01:25:06 +0100

    feat: ajouter la clé API RESEND dans la configuration

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
 		NEXT_PUBLIC_SUPABASE_URL: "https://wmxnxnwivbmywhnsqgya.supabase.co",
 		NEXT_PUBLIC_SUPABASE_ANON_KEY:
 			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndteG54bndpdmJteXdobnNxZ3lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2NjE1MTksImV4cCI6MjA3NzIzNzUxOX0.zB1C1k8_ytIvIR_lLjh74fB7dc8sAJon93bb-I-vjLo",
-		// RESEND_API_KEY: process.env.RESEND_API_KEY,
+		RESEND_API_KEY: "re_HiWCF1od_CvM8L55FmJG5EXRQJ4nAUYjM",
 		// EMAIL_TO: process.env.EMAIL_TO,
 		// EMAIL_FROM: process.env.EMAIL_FROM,
 	},
