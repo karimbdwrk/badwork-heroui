fd82978087dd85826649157f6c39823137e99c95
Karim Badwork <karim@badwork.fr>
2025-12-23 00:20:07 +0100

    feat: ajouter le script Google Tag Manager dans le layout principal

--- app/layout.jsx
+++ app/layout.jsx
@@ -31,8 +31,29 @@ export const viewport = {
 export default function RootLayout({ children }) {
 	return (
 		<html suppressHydrationWarning lang='en'>
-			<head />
+			<head>
+				<Script
+					id='gtm'
+					strategy='afterInteractive'
+					dangerouslySetInnerHTML={{
+						__html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TZNCBWL2');
+            `,
+					}}
+				/>
+			</head>
 			<body>
+				<noscript>
+					<iframe
+						src='https://www.googletagmanager.com/ns.html?id=GTM-TZNCBWL2'
+						height='0'
+						width='0'
+						style='display:none;visibility:hidden'></iframe>
+				</noscript>
 				<Providers
 					themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className='flex flex-col h-screen'>
