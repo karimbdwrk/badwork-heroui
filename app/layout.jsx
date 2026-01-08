19f327cbc6670b41a16f625481de08ec297bc665
Karim Badwork <karim@badwork.fr>
2026-01-08 02:43:29 +0100

    feat: réorganiser le composant Navbar pour un affichage fixe en haut de la page

--- app/layout.jsx
+++ app/layout.jsx
@@ -74,7 +74,16 @@ export default function RootLayout({ children }) {
 					themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<OrientationGuard />
 					<div className='flex flex-col h-screen'>
-						<Navbar />
+						{/* <Navbar /> */}
+						<div
+							style={{
+								position: "fixed",
+								top: 0,
+								width: "100%",
+								zIndex: 1000,
+							}}>
+							<Navbar />
+						</div>
 						<main>{children}</main>
 						<footer className='w-full flex flex-col items-center justify-center py-8'>
 							<Link
