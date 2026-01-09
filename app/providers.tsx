96812651e4638c8c3983ee0fd59f4638e1f2a062
Karim Badwork <karim@badwork.fr>
2026-01-10 00:47:16 +0100

    fix: commenter le composant ToastProvider dans le fournisseur

--- app/providers.tsx
+++ app/providers.tsx
@@ -26,7 +26,7 @@ export function Providers({ children, themeProps }: ProvidersProps) {
 
   return (
     <HeroUIProvider navigate={router.push}>
-      <ToastProvider placement='top-center' />
+      {/* <ToastProvider placement='top-center' /> */}
       <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
     </HeroUIProvider>
   );
