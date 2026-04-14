e55ee4b4eb7b2410f52ea0da1d70482b9359e742
Karim Badwork <karim@badwork.fr>
2026-04-14 18:53:42 +0200

    feat: ajouter des propriétés de viewport pour améliorer la réactivité

--- app/layout.jsx
+++ app/layout.jsx
@@ -38,6 +38,10 @@ const robotoMono = Roboto_Mono({
 });
 
 export const viewport = {
+	width: "device-width",
+	initialScale: 1,
+	maximumScale: 1,
+	userScalable: false,
 	themeColor: [
 		{ media: "(prefers-color-scheme: light)", color: "white" },
 		{ media: "(prefers-color-scheme: dark)", color: "black" },
